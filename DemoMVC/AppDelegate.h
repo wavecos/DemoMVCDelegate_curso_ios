063406381540797b9f321410e07bb520a8db9534
Alfredo Arias <alfre.arias@gmail.com>
2013-10-05 09:41:51 -0400

    Initial Commit

---
+++ DemoMVC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoMVC
+//
+//  Created by Tekhne on 10/5/13.
+//  Copyright (c) 2013 Tekhne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoMVC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DemoMVC
+//
+//  Created by Tekhne on 10/5/13.
+//  Copyright (c) 2013 Tekhne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
